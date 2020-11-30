451c049305553a2f9cdee1b8e679605e70023a15
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2020-11-30 13:22:38 +0200

    feat: reconciliation job
    * keep cmak settings in zk synchornized with helm values
    * prevent manual configuration via UI to break settings from helm values

--- json2zk.py
+++ json2zk.py
@@ -2,6 +2,7 @@ import click
 import logging
 from kazoo.client import KazooClient
 from pathlib import Path
+from hashlib import md5
 
 
 FORMAT = '%(asctime)-15s %(message)s'
@@ -9,22 +10,32 @@ logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 
 @click.command()
+@click.option('--overwrite-zk/--no-overwrite-zk', "over_zk", default=True)
 @click.argument("zk_url", type=str)
 @click.argument("zk_root", type=str)
 @click.argument('json_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False))
-def json2zk(zk_url, zk_root, json_dir):
+def json2zk(over_zk, zk_url, zk_root, json_dir):
 
     zk = KazooClient(hosts=zk_url)
     zk.start()
 
     for json_file in Path(json_dir).glob("*.json"):
+        json_b = json_file.read_bytes()
         dst = f"{zk_root}/{json_file.stem}"
-        logging.info(f"Setting {dst} from {json_file}.")
 
         if zk.exists(dst):
-            zk.set(dst, json_file.read_bytes())
+            file_md5 = md5(json_b).hexdigest()
+
+            zk_b, stat = zk.get(dst)
+            zk_md5 = md5(zk_b).hexdigest()
+            logging.info(f"md5 of {dst}: {zk_md5}, md5 of {json_file}: {file_md5}")
+
+            if zk_md5 != file_md5 and over_zk is True:
+                zk.set(dst, json_b)
+                logging.info(f"Overwritten {dst} from {json_file}")
         else:
-            zk.create(dst, json_file.read_bytes(), makepath=True)
+            zk.create(dst, json_b, makepath=True)
+            logging.info(f"Created {dst} from {json_file}")
 
     zk.stop()
 
