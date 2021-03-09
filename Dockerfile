f8800db5a40a5100dbc9c99160d0b04b3a7e21b8
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2021-03-09 21:43:48 +0200

    feat: extract cmak2zk to standalone tool

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3.8-alpine3.12
 
-COPY *.py /opt/
+COPY *.py /app/bin/
 
 RUN set -x && \
-  apk add --update --no-cache jq curl && \
-  pip install click~=7.0 kazoo~=2.8
+  apk add --verbose --update --no-cache jq curl && \
+  pip install click~=7.0 kazoo~=2.8 zk-shell~=1.3 ruamel.yaml~=0.16 jsonmerge~=1.8
 
-WORKDIR /opt
+WORKDIR /app/
--- cmak2zk.py
+++ cmak2zk.py
@@ -3,42 +3,49 @@ import logging
 from kazoo.client import KazooClient
 from pathlib import Path
 from hashlib import md5
-
+from ruamel.yaml import YAML
+from jsonmerge import merge
+import json
 
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
+
+ZK_ROOT="/kafka-manager/configs"
+
+yaml = YAML(typ='safe')
 
 
 @click.command()
 @click.option('--overwrite-zk/--no-overwrite-zk', "over_zk", default=True)
 @click.argument("zk_url", type=str)
-@click.argument("zk_root", type=str)
-@click.argument('json_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False))
-def cmak2zk(over_zk, zk_url, zk_root, json_dir):
+@click.argument('yaml_cfg', type=click.File())
+def cmak2zk(over_zk, zk_url, yaml_cfg):
+    cmak_cfg = yaml.load(yaml_cfg)['cmak']
+    common_cfg = cmak_cfg['clustersCommon']
 
     zk = KazooClient(hosts=zk_url)
     zk.start()
 
-    for json_file in Path(json_dir).glob("*.json"):
-        json_b = json_file.read_bytes()
-        dst = f"{zk_root}/{json_file.stem}"
+    for cl in cmak_cfg['clusters']:
+        cl = merge(common_cfg, cl)
+        dst = f"{ZK_ROOT}/{cl['name']}"
+        json_b = json.dumps(cl, separators=(',', ':')).encode()
 
         if zk.exists(dst):
             file_md5 = md5(json_b).hexdigest()
 
             zk_b, stat = zk.get(dst)
             zk_md5 = md5(zk_b).hexdigest()
-            logging.info(f"md5 of {dst}: {zk_md5}, md5 of {json_file}: {file_md5}")
+            logging.info(f"md5 of {dst}: {zk_md5}, md5 of {cl['name']}: {file_md5}")
 
             if zk_md5 != file_md5 and over_zk is True:
                 zk.set(dst, json_b)
-                logging.info(f"Overwritten {dst} from {json_file}")
+                logging.info(f"Overwritten {dst} from {yaml_cfg.name}")
         else:
             zk.create(dst, json_b, makepath=True)
-            logging.info(f"Created {dst} from {json_file}")
+            logging.info(f"Created {dst} from {yaml_cfg.name}")
 
     zk.stop()
-
 
 if __name__ == "__main__":
     cmak2zk()
