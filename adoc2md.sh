039ed0918b9b63441437bbcf655bb32a00796372
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2021-03-11 20:38:47 +0200

    chore(docs): readme

--- adoc2md.sh
+++ adoc2md.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# asciidoctor -b docbook -a leveloffset=+1 -o - README.adoc | pandoc --markdown-headings=atx --wrap=preserve -t gfm -f docbook - > README2.md
+asciidoctor -a env-github -b docbook -a leveloffset=+1 -o - README.adoc | pandoc --markdown-headings=atx --wrap=preserve -t gfm -f docbook -
 
-sed -i 's/^plantuml::arch.puml.*$/![Component diagram](http:\/\/www.plantuml.com\/plantuml\/proxy?cache=no\&src=https:\/\/raw.githubusercontent.com\/eshepelyuk\/cmak-operator\/master\/arch.puml)/g' README.md
+# sed -i 's/^plantuml::arch.puml.*$/![Component diagram](http:\/\/www.plantuml.com\/plantuml\/proxy?cache=no\&src=https:\/\/raw.githubusercontent.com\/eshepelyuk\/cmak-operator\/master\/arch.puml)/g' README.md
