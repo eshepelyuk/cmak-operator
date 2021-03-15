5f907f7ccf7070df83c67e7e1c812d6e94eff965
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2021-03-15 19:09:44 +0200

    fix: https in helm readme

--- adoc2md.sh
+++ adoc2md.sh
@@ -2,4 +2,4 @@
 
 # asciidoctor -a env-github -b docbook -a leveloffset=+1 -o - README.adoc | pandoc --markdown-headings=atx --wrap=preserve -t gfm -f docbook -
 
-sed -i 's/^plantuml::arch.puml.*$/![Component diagram](http:\/\/www.plantuml.com\/plantuml\/proxy?cache=no\&src=https:\/\/raw.githubusercontent.com\/eshepelyuk\/cmak-operator\/master\/arch.puml)/g' README.md
+sed -i 's/^plantuml::arch.puml.*$/![Component diagram](https:\/\/www.plantuml.com\/plantuml\/proxy?cache=no\&src=https:\/\/raw.githubusercontent.com\/eshepelyuk\/cmak-operator\/master\/arch.puml)/g' README.md
