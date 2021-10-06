b4b638928f8e82b3a93631e4c3764002f42eab23
dmakeroam <cpfapp@gmail.com>
2021-10-06 16:35:16 +0700

    feat: add nodeSelector, pod tolerations, node affinity configuration

--- test/linter/test.sh
+++ test/linter/test.sh
@@ -4,3 +4,25 @@ helm lint . --strict
 helm lint . --strict -f test/linter/values-lint.yaml
 helm lint . --strict -f test/linter/values-ssl.yaml
 helm dep up test/linter/subchart && helm lint test/linter/subchart --strict
+
+helm lint . --strict --set 'tolerations.sample=1'
+if [ $? -eq 0 ]; then
+  exit 1
+fi
+
+helm lint . --strict --set 'tolerations.sample={"qwe", "asd"}'
+if [ $? -eq 0 ]; then
+  exit 1
+fi
+
+helm lint . --strict --set 'nodeSelector={1,2,3}'
+if [ $? -eq 0 ]; then
+  exit 1
+fi
+
+helm lint . --strict --set 'affinity={1,2,3}'
+if [ $? -eq 0 ]; then
+  exit 1
+fi
+
+
