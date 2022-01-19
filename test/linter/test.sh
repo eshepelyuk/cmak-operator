59dffd19ee5d1a27e54b7b79564c91225f8e3e5b
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2022-01-19 23:03:56 +0200

    fix: linter test

--- test/linter/test.sh
+++ test/linter/test.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 helm lint . --strict
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
 helm lint . --strict -f test/linter/values-lint.yaml
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
 helm lint . --strict -f test/linter/values-ssl.yaml
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
 helm dep up test/linter/subchart && helm lint test/linter/subchart --strict
+if [ $? -ne 0 ]; then
+  exit -1
+fi
 
 helm lint . --strict --set 'tolerations.sample=1'
 if [ $? -eq 0 ]; then
